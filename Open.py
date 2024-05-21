1bbd678137828749c4be734479122353fb392aa3
klaus <oriokiklaus@gmail.com>
2024-05-21 19:50:45 +0300

    add code to self trainable bot, data adds to json knowledgebase when prompted and returns most likely answer

---
+++ Open.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key = "sk-proj-ocZY16DoSTfHo5zhahEfT3BlbkFJmJ89qxoSReHM2CKxelUi"
+)
+
+def chat_with_gpt(prompt):
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit","exit","byw"]:
+            break
+        response = chat_with_gpt(user_input)
+        print("chatbot: ", response)
